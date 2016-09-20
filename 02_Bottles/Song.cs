459a93282c103d8d97b3d0380fcd4b23b9f263a9
stef801f <stef801f@edu.eal.dk>
2016-09-20 18:52:22 +0200

    Add project files.

---
+++ 02_Bottles/Song.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02_Bottles
+{
+    internal class Song
+    {
+        internal string CountBottles(int v)
+        {
+            if (v == 0) 
+                return " No more bottles of beer on the wall.";
+            if (v == 1)
+                return "1 bottle of beer on the wall."
+                + " 1 bottle of beer."
+                + " Take one down and pass it around."
+                + " No more bottles of beer on the wall.";
+            if (v == 2)
+                return "2 bottles of beer on the wall."
+                + " 2 bottles of beer."
+                + " Take one down and pass it around."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer."
+                + " Take one down and pass it around."
+                + " No more bottles of beer on the wall.";
+            if (v == 3)
+                return "3 bottles of beer on the wall."
+                + " 3 bottles of beer."
+                + " Take one down and pass it around."
+                + " 2 bottles of beer on the wall."
+                + " 2 bottles of beer on the wall."
+                + " 2 bottles of beer."
+                + " Take one down and pass it around."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer on the wall."
+                + " 1 bottle of beer."
+                + " Take one down and pass it around."
+                + " No more bottles of beer on the wall.";
+            return "";
+        }
+    }
+}
